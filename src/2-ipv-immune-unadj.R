e5b95fc25c751a4801874685b1c7a0ba149008c9
Andrew Mertens <amertens@berkeley.edu>
2021-03-08 06:28:33 -0800

    init commit

---
+++ src/2-ipv-immune-unadj.R
@@ -0,0 +1,182 @@
+rm(list=ls())
+ 
+source(here::here("0-config.R"))
+#devtools::install_github("washb-eed-substudies/washbgam")
+#source(here::here("src/0-gam-functions.R"))
+
+d <- read.csv(paste0(dropboxDir,"Data/Cleaned/Audrie/bangladesh-dm-ee-ipv-cesd-pss-covariates-immunelab.csv"))
+colnames(d)
+#### Loop over exposure-outcome pairs ####
+
+#### Hypothesis 1:
+# 1.	Maternal exposure to IPV during pregnancy, IPV during the child's first year of life, 
+#and cumulative lifetime exposure to IPV at Year 2 is associated with child systemic inflammation at 
+#Years 1 and 2 (defined as higher concentrations of pro-inflammatory factors and/or lower concentrations 
+#of anti-inflammatory/immuno-regulatory factors).
+# Exposures: Maternal exposure to IPV during pregnancy, IPV during the child's first year of life, and 
+#cumulative lifetime exposure to IPV at Year 2 as measured by the WHO Women's Health and Life Experiences Survey
+Xvars <- c("viol_any_preg","viol_any_t2","life_viol_any_t3")            
+
+# Outcomes: Child sum score of systemic inflammation, Th1/Th2, 
+#Th1/Th17, Th1/IL-10, Th2/IL-10, Th17/IL-10, 
+#GM-CSF/IL-10, and IL-2/IL-10 cytokine ratios, IGF-1, CRP, and AGP at Years 1 and 2.
+Yvars <- c("cesd_sum_t2","cesd_sum_ee_t3","t2_ratio_th1_th2","t3_ratio_th1_th2",
+           "t2_ratio_th1_th17","t3_ratio_th1_th17", "t2_ratio_th1_il10","t3_ratio_th1_il10",
+           "t2_ratio_th2_il10","t3_ratio_th2_il10", "t2_ratio_th17_il10","t3_ratio_th17_il10",
+           "t2_ratio_gmc_il10","t3_ratio_gmc_il10", "t2_ratio_il12_il10","t3_ratio_il12_il10",
+           "igf_t2","igf_t3","crp_t2","crp_t3","agp_t2","agp_t3")
+           
+
+#Fit models
+H1_models <- NULL
+for(i in Xvars){
+  for(j in Yvars){
+    cat(i,"\n")
+    cat(j,"\n")
+    res_unadj <- fit_RE_gam(d=d, X=i, Y=j,  W=NULL)
+    res <- data.frame(X=i, Y=j, fit=I(list(res_unadj$fit)), dat=I(list(res_unadj$dat)))
+    H1_models <- bind_rows(H1_models, res)
+  }
+}
+
+#Get primary contrasts
+H1_res <- NULL
+for(i in 1:nrow(H1_models)){
+  res <- data.frame(X=H1_models$X[i], Y=H1_models$Y[i])
+  preds <- predict_gam_diff(fit=H1_models$fit[i][[1]], d=H1_models$dat[i][[1]], quantile_diff=c(0.25,0.75), Xvar=res$X, Yvar=res$Y)
+  H1_res <-  bind_rows(H1_res , preds$res)
+}
+
+#Make list of plots
+H1_plot_list <- NULL
+H1_plot_data <- NULL
+for(i in 1:nrow(H1_models)){
+  res <- data.frame(X=H1_models$X[i], Y=H1_models$Y[i])
+  simul_plot <- gam_simul_CI(H1_models$fit[i][[1]], H1_models$dat[i][[1]], xlab=res$X, ylab=res$Y, title="")
+  H1_plot_list[[i]] <-  simul_plot$p
+  H1_plot_data <-  rbind(H1_plot_data, data.frame(Xvar=res$X, Yvar=res$Y, adj=0, simul_plot$pred))
+}
+
+
+#Save models
+saveRDS(H1_models, here("models/H1_models.RDS"))
+
+#Save results
+saveRDS(H1_res, here("results/unadjusted/H1_res.RDS"))
+
+
+#Save plots
+#saveRDS(H1_plot_list, here("figure-objects/H1_unadj_splines.RDS"))
+
+#Save plot data
+saveRDS(H1_plot_data, here("figure-data/H1_unadj_spline_data.RDS"))
+
+
+
+#### Hypothesis 2: 
+# 2.	Parental stress measured at Year 2 is associated with concurrent child systemic inflammation (defined as higher concentrations of pro-inflammatory factors and/or lower concentrations of anti-inflammatory/immuno-regulatory factors).
+# Exposures: Maternal and paternal Perceived Stress Scale (PSS) scores at Year 2
+Xvars <- c("pss_sum_mom_t3", "pss_sum_dad_t3")
+# Outcomes: Child sum score of systemic inflammation, Th1/Th2, Th1/Th17, Th1/IL-10, Th2/IL-10, Th17/IL-10, GM-CSF/IL-10, and 
+#IL-2/IL-10 cytokine ratios, IGF-1, CRP, and AGP at Year 2.
+Yvars <- c("cesd_sum_t2","cesd_sum_ee_t3","t2_ratio_th1_th2","t3_ratio_th1_th2",
+           "t2_ratio_th1_th17","t3_ratio_th1_th17", "t2_ratio_th1_il10","t3_ratio_th1_il10",
+           "t2_ratio_th2_il10","t3_ratio_th2_il10", "t2_ratio_th17_il10","t3_ratio_th17_il10",
+           "t2_ratio_gmc_il10","t3_ratio_gmc_il10", "t2_ratio_il12_il10","t3_ratio_il12_il10",
+           "igf_t2","igf_t3","crp_t2","crp_t3","agp_t2","agp_t3")
+#Fit models
+H2_models <- NULL
+for(i in Xvars){
+  for(j in Yvars){
+    res_unadj <- fit_RE_gam(d=d, X=i, Y=j,  W=NULL)
+    res <- data.frame(X=i, Y=j, fit=I(list(res_unadj$fit)), dat=I(list(res_unadj$dat)))
+    H2_models <- bind_rows(H2_models, res)
+  }
+}
+
+#Get primary contrasts
+H2_res <- NULL
+for(i in 1:nrow(H2_models)){
+  res <- data.frame(X=H2_models$X[i], Y=H2_models$Y[i])
+  preds <- predict_gam_diff(fit=H2_models$fit[i][[1]], d=H2_models$dat[i][[1]], quantile_diff=c(0.25,0.75), Xvar=res$X, Yvar=res$Y)
+  H2_res <-  bind_rows(H2_res , preds$res)
+}
+
+#Make list of plots
+H2_plot_list <- NULL
+H2_plot_data <- NULL
+for(i in 1:nrow(H2_models)){
+  res <- data.frame(X=H2_models$X[i], Y=H2_models$Y[i])
+  simul_plot <- gam_simul_CI(H2_models$fit[i][[1]], H2_models$dat[i][[1]], xlab=res$X, ylab=res$Y, title="")
+  H2_plot_list[[i]] <-  simul_plot$p
+  H2_plot_data <-  rbind(H2_plot_data, data.frame(Xvar=res$X, Yvar=res$Y, adj=0, simul_plot$pred))
+}
+
+
+#Save models
+saveRDS(H2_models, here("models/H2_models.RDS"))
+
+#Save results
+saveRDS(H2_res, here("results/unadjusted/H2_res.RDS"))
+
+
+#Save plots
+#saveRDS(H2_plot_list, here("figure-objects/H2_unadj_splines.RDS"))
+
+#Save plot data
+saveRDS(H2_plot_data, here("figure-data/H2_unadj_spline_data.RDS"))
+
+
+
+#### Hypothesis 3: 
+# 3.	Maternal depressive symptoms, measured on a continuous scale at Years 1 and 2, is associated with child systemic inflammation at Years 1 and 2 (defined as higher concentrations of pro-inflammatory factors and/or lower concentrations of anti-inflammatory/immuno-regulatory factors). 
+# Exposures: Maternal Center for Epidemiologic Studies (CES-D) Scale score at Years 1 and 2
+Xvars <- c("cesd_sum_t2","cesd_sum_ee_t3")
+# Outcomes: Child sum score of systemic inflammation, Th1/Th2, Th1/Th17, Th1/IL-10, Th2/IL-10, Th17/IL-10, GM-CSF/IL-10, and IL-2/IL-10 cytokine ratios, IGF-1, CRP, and AGP at Years 1 and 2.
+Yvars <- c("cesd_sum_t2","cesd_sum_ee_t3","t2_ratio_th1_th2","t3_ratio_th1_th2",
+           "t2_ratio_th1_th17","t3_ratio_th1_th17", "t2_ratio_th1_il10","t3_ratio_th1_il10",
+           "t2_ratio_th2_il10","t3_ratio_th2_il10", "t2_ratio_th17_il10","t3_ratio_th17_il10",
+           "t2_ratio_gmc_il10","t3_ratio_gmc_il10", "t2_ratio_il12_il10","t3_ratio_il12_il10",
+           "igf_t2","igf_t3","crp_t2","crp_t3","agp_t2","agp_t3")
+#Fit models
+H3_models <- NULL
+for(i in Xvars){
+  for(j in Yvars){
+    res_unadj <- fit_RE_gam(d=d, X=i, Y=j,  W=NULL)
+    res <- data.frame(X=i, Y=j, fit=I(list(res_unadj$fit)), dat=I(list(res_unadj$dat)))
+    H3_models <- bind_rows(H3_models, res)
+  }
+}
+
+#Get primary contrasts
+H3_res <- NULL
+for(i in 1:nrow(H3_models)){
+  res <- data.frame(X=H3_models$X[i], Y=H3_models$Y[i])
+  preds <- predict_gam_diff(fit=H3_models$fit[i][[1]], d=H3_models$dat[i][[1]], quantile_diff=c(0.25,0.75), Xvar=res$X, Yvar=res$Y)
+  H3_res <-  bind_rows(H3_res , preds$res)
+}
+
+#Make list of plots
+H3_plot_list <- NULL
+H3_plot_data <- NULL
+for(i in 1:nrow(H3_models)){
+  res <- data.frame(X=H3_models$X[i], Y=H3_models$Y[i])
+  simul_plot <- gam_simul_CI(H3_models$fit[i][[1]], H3_models$dat[i][[1]], xlab=res$X, ylab=res$Y, title="")
+  H3_plot_list[[i]] <-  simul_plot$p
+  H3_plot_data <-  rbind(H3_plot_data, data.frame(Xvar=res$X, Yvar=res$Y, adj=0, simul_plot$pred))
+}
+
+
+#Save models
+saveRDS(H3_models, here("models/H3_models.RDS"))
+
+#Save results
+saveRDS(H3_res, here("results/unadjusted/H3_res.RDS"))
+
+
+#Save plots
+#saveRDS(H3_plot_list, here("figure-objects/H3_unadj_splines.RDS"))
+
+#Save plot data
+saveRDS(H3_plot_data, here("figure-data/H3_unadj_spline_data.RDS"))
+
---
+++ src/3-ipv-immune-adj.R
@@ -0,0 +1,266 @@
+rm(list=ls())
+
+source(here::here("0-config.R"))
+#devtools::install_github("washb-eed-substudies/washbgam")
+#source(here::here("src/0-gam-functions.R"))
+
+d <- read.csv(paste0(dropboxDir,"Data/Cleaned/Audrie/bangladesh-dm-ee-ipv-cesd-pss-covariates-immunelab.csv"))
+colnames(d)
+
+
+#---------------------------------------------------------------
+# set covariates
+#---------------------------------------------------------------
+
+# .	Enrollment characteristics:
+#   o	Child sex
+# o	Child birth order (first born, second born or greater)
+# o	Mother's age (years)
+# o	Mother's height (cm)
+# o	Mother's education level (no education, primary, secondary)
+# o	Household food insecurity (4-level HFIAS categories) 
+# o	Number of children < 18 years in the household
+# o	Number of individuals living in the compound
+# o	Distance (in minutes) to the household's primary drinking water source
+# o	Housing materials (floor, walls, roof)
+# o	Asset-based household-wealth variable (continuous), calculated from the first principal component of a principal components analysis of the following household assets: electricity, wardrobe, table, chair or bench, khat, chouki, working radio, working black/white or color television, refrigerator, bicycle, motorcycle, sewing machine, mobile phone, land phone, number of cows, number of goats, number of chickens.
+# 
+# We will also test characteristics measured during follow-up (see directed acyclic graph (DAG) in Figure 1 below):
+# .	Month of measurement 
+# .	Child age (days)
+# .	Treatment arm (control or combined nutrition, water, sanitation, and handwashing intervention (N+WSH)).    
+
+
+#Set list of adjustment variables
+#Make vectors of adjustment variable names
+#removed roof because of low variability
+Wvars<-c("sex","birthord", "momage","momheight","momedu", 
+         "hfiacat", "Nlt18","Ncomp", "watmin", "walls", 
+         "floor", "HHwealth", "tr", "cesd_sum_t2", 
+         "ari7d_t2", "diar7d_t2", "nose7d_t2", "life_viol_any_t3")
+
+Wvars[!(Wvars %in% colnames(d))]
+
+
+
+#Add in time varying covariates:
+Wvars2<-c("ageday_bt2", "ageday_at2",  "month_bt2", "month_at2", "laz_t1_cat", "waz_t1_cat") 
+Wvars3<-c("ageday_bt3", "ageday_at3", "month_bt3", "month_at3", 
+          "laz_t2_cat", "waz_t2_cat", "cesd_sum_ee_t3", "pss_sum_mom_t3", 
+          "ari7d_t3", "diar7d_t3", "nose7d_t3") 
+Wvars23<-c("ageday_bt2", "ageday_at3", "month_bt2", "month_at3", 
+           "laz_t2", "waz_t2", "cesd_sum_ee_t3", "pss_sum_mom_t3", 
+           "ari7d_t3", "diar7d_t3", "nose7d_t3")
+Wvars_anthro23<-c("ageday_bt2", "ageday_at2", "ageday_at3", "month_bt2", "month_at2", "month_at3", 
+                  "cesd_sum_ee_t3", "pss_sum_mom_t3", "ari7d_t3", "diar7d_t3", "nose7d_t3")
+
+W2_immmune.W2_anthro <- c(Wvars, Wvars2) %>% unique(.)
+W3_immune.W3_anthro <- c(Wvars, Wvars3) %>% unique(.)
+W2_immune.W3_anthro <- c(Wvars, Wvars23) %>% unique(.)
+W2_immune.W23_anthro <- c(Wvars, Wvars_anthro23) %>% unique(.)
+
+add_hcz <- function(i, j, W){
+  if (j=="hcz_t3"){
+    if(grepl("t2", i)){Wset=c(W, "hcz_t2")}
+    else {Wset=c(W, "hcz_t2_cat")}}
+  else if (j=="hcz_t2"){Wset=c(W, "hcz_t1_cat")}
+  else {Wset=W}
+  return(Wset)
+}
+
+#### Hypothesis 1: immune status associated with concurrent child growth ####
+# all immune ratios at Y1 v. growth outcomes at Y1
+Xvars <- c("t2_ratio_pro_il10", "t2_ratio_il2_il10", "t2_ratio_gmc_il10", "t2_ratio_th1_il10", "t2_ratio_th2_il10",     
+           "t2_ratio_th17_il10", "t2_ratio_th1_th2", "t2_ratio_th1_th17", "t2_ln_agp", "t2_ln_crp", "sumscore_t2_Z", "t2_ln_ifn")
+Yvars <- c("laz_t2", "waz_t2", "whz_t2" ,"hcz_t2") 
+
+#Fit models
+H1_adj_nofever_models <- NULL
+for(i in Xvars){
+  for(j in Yvars){
+    print(i)
+    print(j)
+    Wset <- add_hcz(i, j, W2_immmune.W2_anthro)
+    res_adj <- fit_RE_gam(d=d, X=i, Y=j,  W=Wset)
+    res <- data.frame(X=i, Y=j, fit=I(list(res_adj$fit)), dat=I(list(res_adj$dat)))
+    H1_adj_nofever_models <- bind_rows(H1_adj_nofever_models, res)
+  }
+}
+
+
+#---------------------------------------------------------------
+#### Loop over exposure-outcome pairs ####
+#---------------------------------------------------------------
+
+#### Hypothesis 1:
+# 1.	Maternal exposure to IPV during pregnancy, IPV during the child's first year of life, 
+#and cumulative lifetime exposure to IPV at Year 2 is associated with child systemic inflammation at 
+#Years 1 and 2 (defined as higher concentrations of pro-inflammatory factors and/or lower concentrations 
+#of anti-inflammatory/immuno-regulatory factors).
+# Exposures: Maternal exposure to IPV during pregnancy, IPV during the child's first year of life, and 
+#cumulative lifetime exposure to IPV at Year 2 as measured by the WHO Women's Health and Life Experiences Survey
+Xvars <- c("viol_any_preg","viol_any_t2","life_viol_any_t3")            
+
+# Outcomes: Child sum score of systemic inflammation, Th1/Th2, 
+#Th1/Th17, Th1/IL-10, Th2/IL-10, Th17/IL-10, 
+#GM-CSF/IL-10, and IL-2/IL-10 cytokine ratios, IGF-1, CRP, and AGP at Years 1 and 2.
+Yvars <- c("cesd_sum_t2","cesd_sum_ee_t3","t2_ratio_th1_th2","t3_ratio_th1_th2",
+           "t2_ratio_th1_th17","t3_ratio_th1_th17", "t2_ratio_th1_il10","t3_ratio_th1_il10",
+           "t2_ratio_th2_il10","t3_ratio_th2_il10", "t2_ratio_th17_il10","t3_ratio_th17_il10",
+           "t2_ratio_gmc_il10","t3_ratio_gmc_il10", "t2_ratio_il12_il10","t3_ratio_il12_il10",
+           "igf_t2","igf_t3","crp_t2","crp_t3","agp_t2","agp_t3")
+
+
+#Fit models
+H1_models <- NULL
+for(i in Xvars){
+  for(j in Yvars){
+    cat(i,"\n")
+    cat(j,"\n")
+    res_adj <- fit_RE_gam(d=d, X=i, Y=j,  W=NULL)
+    res <- data.frame(X=i, Y=j, fit=I(list(res_adj$fit)), dat=I(list(res_adj$dat)))
+    H1_models <- bind_rows(H1_models, res)
+  }
+}
+
+#Get primary contrasts
+H1_res <- NULL
+for(i in 1:nrow(H1_models)){
+  res <- data.frame(X=H1_models$X[i], Y=H1_models$Y[i])
+  preds <- predict_gam_diff(fit=H1_models$fit[i][[1]], d=H1_models$dat[i][[1]], quantile_diff=c(0.25,0.75), Xvar=res$X, Yvar=res$Y)
+  H1_res <-  bind_rows(H1_res , preds$res)
+}
+
+#Make list of plots
+H1_plot_list <- NULL
+H1_plot_data <- NULL
+for(i in 1:nrow(H1_models)){
+  res <- data.frame(X=H1_models$X[i], Y=H1_models$Y[i])
+  simul_plot <- gam_simul_CI(H1_models$fit[i][[1]], H1_models$dat[i][[1]], xlab=res$X, ylab=res$Y, title="")
+  H1_plot_list[[i]] <-  simul_plot$p
+  H1_plot_data <-  rbind(H1_plot_data, data.frame(Xvar=res$X, Yvar=res$Y, adj=0, simul_plot$pred))
+}
+
+
+#Save models
+saveRDS(H1_models, here("models/H1_models.RDS"))
+
+#Save results
+saveRDS(H1_res, here("results/H1_res.RDS"))
+
+
+#Save plots
+#saveRDS(H1_plot_list, here("figure-objects/H1_adj_splines.RDS"))
+
+#Save plot data
+saveRDS(H1_plot_data, here("figure-data/H1_adj_spline_data.RDS"))
+
+
+
+#### Hypothesis 2: 
+# 2.	Parental stress measured at Year 2 is associated with concurrent child systemic inflammation (defined as higher concentrations of pro-inflammatory factors and/or lower concentrations of anti-inflammatory/immuno-regulatory factors).
+# Exposures: Maternal and paternal Perceived Stress Scale (PSS) scores at Year 2
+Xvars <- c("pss_sum_mom_t3", "pss_sum_dad_t3")
+# Outcomes: Child sum score of systemic inflammation, Th1/Th2, Th1/Th17, Th1/IL-10, Th2/IL-10, Th17/IL-10, GM-CSF/IL-10, and 
+#IL-2/IL-10 cytokine ratios, IGF-1, CRP, and AGP at Year 2.
+Yvars <- c("cesd_sum_t2","cesd_sum_ee_t3","t2_ratio_th1_th2","t3_ratio_th1_th2",
+           "t2_ratio_th1_th17","t3_ratio_th1_th17", "t2_ratio_th1_il10","t3_ratio_th1_il10",
+           "t2_ratio_th2_il10","t3_ratio_th2_il10", "t2_ratio_th17_il10","t3_ratio_th17_il10",
+           "t2_ratio_gmc_il10","t3_ratio_gmc_il10", "t2_ratio_il12_il10","t3_ratio_il12_il10",
+           "igf_t2","igf_t3","crp_t2","crp_t3","agp_t2","agp_t3")
+#Fit models
+H2_models <- NULL
+for(i in Xvars){
+  for(j in Yvars){
+    res_adj <- fit_RE_gam(d=d, X=i, Y=j,  W=NULL)
+    res <- data.frame(X=i, Y=j, fit=I(list(res_adj$fit)), dat=I(list(res_adj$dat)))
+    H2_models <- bind_rows(H2_models, res)
+  }
+}
+
+#Get primary contrasts
+H2_res <- NULL
+for(i in 1:nrow(H2_models)){
+  res <- data.frame(X=H2_models$X[i], Y=H2_models$Y[i])
+  preds <- predict_gam_diff(fit=H2_models$fit[i][[1]], d=H2_models$dat[i][[1]], quantile_diff=c(0.25,0.75), Xvar=res$X, Yvar=res$Y)
+  H2_res <-  bind_rows(H2_res , preds$res)
+}
+
+#Make list of plots
+H2_plot_list <- NULL
+H2_plot_data <- NULL
+for(i in 1:nrow(H2_models)){
+  res <- data.frame(X=H2_models$X[i], Y=H2_models$Y[i])
+  simul_plot <- gam_simul_CI(H2_models$fit[i][[1]], H2_models$dat[i][[1]], xlab=res$X, ylab=res$Y, title="")
+  H2_plot_list[[i]] <-  simul_plot$p
+  H2_plot_data <-  rbind(H2_plot_data, data.frame(Xvar=res$X, Yvar=res$Y, adj=0, simul_plot$pred))
+}
+
+
+#Save models
+saveRDS(H2_models, here("models/H2_models.RDS"))
+
+#Save results
+saveRDS(H2_res, here("results/H2_res.RDS"))
+
+
+#Save plots
+#saveRDS(H2_plot_list, here("figure-objects/H2_adj_splines.RDS"))
+
+#Save plot data
+saveRDS(H2_plot_data, here("figure-data/H2_adj_spline_data.RDS"))
+
+
+
+#### Hypothesis 3: 
+# 3.	Maternal depressive symptoms, measured on a continuous scale at Years 1 and 2, is associated with child systemic inflammation at Years 1 and 2 (defined as higher concentrations of pro-inflammatory factors and/or lower concentrations of anti-inflammatory/immuno-regulatory factors). 
+# Exposures: Maternal Center for Epidemiologic Studies (CES-D) Scale score at Years 1 and 2
+Xvars <- c("cesd_sum_t2","cesd_sum_ee_t3")
+# Outcomes: Child sum score of systemic inflammation, Th1/Th2, Th1/Th17, Th1/IL-10, Th2/IL-10, Th17/IL-10, GM-CSF/IL-10, and IL-2/IL-10 cytokine ratios, IGF-1, CRP, and AGP at Years 1 and 2.
+Yvars <- c("cesd_sum_t2","cesd_sum_ee_t3","t2_ratio_th1_th2","t3_ratio_th1_th2",
+           "t2_ratio_th1_th17","t3_ratio_th1_th17", "t2_ratio_th1_il10","t3_ratio_th1_il10",
+           "t2_ratio_th2_il10","t3_ratio_th2_il10", "t2_ratio_th17_il10","t3_ratio_th17_il10",
+           "t2_ratio_gmc_il10","t3_ratio_gmc_il10", "t2_ratio_il12_il10","t3_ratio_il12_il10",
+           "igf_t2","igf_t3","crp_t2","crp_t3","agp_t2","agp_t3")
+#Fit models
+H3_models <- NULL
+for(i in Xvars){
+  for(j in Yvars){
+    res_adj <- fit_RE_gam(d=d, X=i, Y=j,  W=NULL)
+    res <- data.frame(X=i, Y=j, fit=I(list(res_adj$fit)), dat=I(list(res_adj$dat)))
+    H3_models <- bind_rows(H3_models, res)
+  }
+}
+
+#Get primary contrasts
+H3_res <- NULL
+for(i in 1:nrow(H3_models)){
+  res <- data.frame(X=H3_models$X[i], Y=H3_models$Y[i])
+  preds <- predict_gam_diff(fit=H3_models$fit[i][[1]], d=H3_models$dat[i][[1]], quantile_diff=c(0.25,0.75), Xvar=res$X, Yvar=res$Y)
+  H3_res <-  bind_rows(H3_res , preds$res)
+}
+
+#Make list of plots
+H3_plot_list <- NULL
+H3_plot_data <- NULL
+for(i in 1:nrow(H3_models)){
+  res <- data.frame(X=H3_models$X[i], Y=H3_models$Y[i])
+  simul_plot <- gam_simul_CI(H3_models$fit[i][[1]], H3_models$dat[i][[1]], xlab=res$X, ylab=res$Y, title="")
+  H3_plot_list[[i]] <-  simul_plot$p
+  H3_plot_data <-  rbind(H3_plot_data, data.frame(Xvar=res$X, Yvar=res$Y, adj=0, simul_plot$pred))
+}
+
+
+#Save models
+saveRDS(H3_models, here("models/H3_models.RDS"))
+
+#Save results
+saveRDS(H3_res, here("results/H3_res.RDS"))
+
+
+#Save plots
+#saveRDS(H3_plot_list, here("figure-objects/H3_adj_splines.RDS"))
+
+#Save plot data
+saveRDS(H3_plot_data, here("figure-data/H3_adj_spline_data.RDS"))
+
